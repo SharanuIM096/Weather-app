f55e9603a37f1bc98d8d1b09e50b9e4da8a4c5d0
SharanappaMelmuri <imsharan096@gmail.com>
2023-07-13 12:36:56 +0530

    first commit

---
+++ src/component/Weather.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+const Weather = () => {
+    
+    let [placeid,setplaceid] = useState(null);
+    let [currentweather,setcurrentweather] = useState(null);
+    let [time,settime]=useState("");
+   
+    
+   let handleweather =()=>{
+    const url = `https://ai-weather-by-meteosource.p.rapidapi.com/current?place_id=${placeid}&timezone=auto&language=en&units=auto`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ca5a3b3177msh3e7eb36082dfdf1p11be78jsnf65b5c3dd62c',
+            'X-RapidAPI-Host': 'ai-weather-by-meteosource.p.rapidapi.com'
+        }
+    };
+      fetch(url, options)
+    .then((res)=>{return res.json()})
+    .then((data)=>{setcurrentweather(data.current) ;console.log(data); settime(data.timezone) })
+   }
+    return ( 
+        
+        <div className="weather">
+            <h2>***Weather Application***</h2>
+
+         <div className="search">
+
+         <input type="text" onChange={(e)=>{setplaceid(e.target.value)}} placeholder="Enter the place name"/>
+         <button onClick={handleweather}>Search</button>
+
+         </div>
+
+      
+       {currentweather  && <div className="deatils">
+        <h2 align="right">{placeid}</h2>
+        <h2 align="right">{time}</h2>
+        <h2 align="right">  {currentweather.temperature} <sup>o</sup></h2>
+        <h2 align="right">{currentweather.summary}</h2>
+        <h2 align="right">{currentweather.timezone}</h2>
+       <div className="spans"> <span>Wind direction : {currentweather.wind.dir}</span> <br />
+        <span>Humidity : {currentweather.humidity}</span><br />
+        <span>Cloud cover : {currentweather.cloud_cover}</span><br />
+        <span>Dew Point : {currentweather.dew_point}</span><br />
+        <span>Pressure : {currentweather.pressure}</span><br />
+       
+        <span>visibility : {currentweather.visibility}</span></div>
+      </div> }
+
+
+        </div>
+
+       
+
+
+    )
+     ;
+}
+ 
+export default Weather;
